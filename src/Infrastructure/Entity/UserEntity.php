0f5b97379a1c65eb95b2887839e8d0ec9c8a232a
Piotr Macha <me@piotrmacha.pl>
2018-03-19 21:04:06 +0100

    Refactor Doctrine layer and create Doctrine entity as a DTO between database and domain layer

--- src/Infrastructure/Entity/UserEntity.php
+++ src/Infrastructure/Entity/UserEntity.php
@@ -9,15 +9,57 @@
 namespace App\Infrastructure\Entity;
 
 
+use App\Domain\Factory\UserFactory;
+use App\Domain\Model\CountryCode;
 use App\Domain\Model\User;
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="users")
+ */
 class UserEntity
 {
-    public static function fromDomainObject(User $user) {
+    /**
+     * @ORM\Column(name="user_id")
+     */
+    private $id;
+    /**
+     * @ORM\Column(name="identification_number")
+     */
+    private $identificationNumber;
+    /**
+     * @ORM\Column(name="first_name")
+     */
+    private $firstName;
+    /**
+     * @ORM\Column(name="surname")
+     */
+    private $surname;
+    /**
+     * @ORM\Column(name="country_code")
+     */
+    private $countryCode;
 
+    public static function fromDomainObject(User $user): UserEntity {
+        $self = new self();
+
+        $self->id = $user->userId();
+        $self->identificationNumber = $user->identityNumber()->value();
+        $self->firstName = $user->name()->firstName();
+        $self->surname = $user->name()->surname();
+        $self->countryCode = $user->countryCode()->getValue();
+
+        return $self;
     }
 
-    public function toDomainObject() {
-
+    public function toDomainObject(UserFactory $factory): User {
+        return $factory->createWithId(
+            $this->id,
+            $this->identificationNumber,
+            CountryCode::get($this->countryCode),
+            $this->firstName,
+            $this->surname
+        );
     }
 }
--- src/Infrastructure/Repository/DoctrineUserRepository.php
+++ src/Infrastructure/Repository/DoctrineUserRepository.php
@@ -9,8 +9,10 @@
 namespace App\Infrastructure\Repository;
 
 
+use App\Domain\Factory\UserFactory;
 use App\Domain\Model\User;
 use App\Domain\Repository\UserRepository;
+use App\Infrastructure\Entity\UserEntity;
 use Doctrine\ORM\EntityManagerInterface;
 
 class DoctrineUserRepository implements UserRepository
@@ -19,15 +21,20 @@ class DoctrineUserRepository implements UserRepository
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var UserFactory
+     */
+    private $factory;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserFactory $factory)
     {
         $this->entityManager = $entityManager;
+        $this->factory = $factory;
     }
 
     public function store(User $user)
     {
-        $this->entityManager->persist($user);
+        $this->entityManager->persist(UserEntity::fromDomainObject($user));
         $this->entityManager->flush();
     }
 
